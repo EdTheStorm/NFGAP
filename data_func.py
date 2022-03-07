2ab90f27b77dac7fb2f288fbfa0e44f4236204f5
EdTheStorm <75705636+EdTheStorm@users.noreply.github.com>
2022-03-07 11:16:54 +0100

    Add files via upload

---
+++ data_func.py
@@ -0,0 +1,48 @@
+
+from io import StringIO
+
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfparser import PDFParser
+
+def convert_pdf_to_string(file_path):
+    #generic text extractor code copied from the pdfminer.six documentation, and slightly modified so we can use it as a function
+
+	output_string = StringIO()
+	with open(file_path, 'rb') as in_file:
+	    parser = PDFParser(in_file)
+	    doc = PDFDocument(parser)
+	    rsrcmgr = PDFResourceManager()
+	    device = TextConverter(rsrcmgr, output_string, laparams=LAParams())
+	    interpreter = PDFPageInterpreter(rsrcmgr, device)
+	    for page in PDFPage.create_pages(doc):
+	        interpreter.process_page(page)
+
+	return(output_string.getvalue())
+
+                
+def convert_title_to_filename(title): #a function that takes the title as it appears in the table of contents, and converts it to the name of the file
+    filename = title.lower()
+    filename = filename.replace(' ', '_')
+    return filename
+
+
+def split_to_title_and_pagenum(table_of_contents_entry): #used to determine if a line in the table of contents is actually a title — page number pair, and if yes, returns them in a tuple
+    title_and_pagenum = table_of_contents_entry.strip()
+    
+    title = None
+    pagenum = None
+    
+    if len(title_and_pagenum) > 0:
+        if title_and_pagenum[-1].isdigit():
+            i = -2
+            while title_and_pagenum[i].isdigit():
+                i -= 1
+
+            title = title_and_pagenum[:i].strip()
+            pagenum = int(title_and_pagenum[i:].strip())
+        
+    return title, pagenum
